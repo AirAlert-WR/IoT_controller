6ac7fc1b46901d24ed0ea8f9fdc88743505b9c35
Данила Блойль <danilobleul2005@gmail.com>
2025-06-13 13:21:49 +0200

    Removed old sensors

--- test/test_sensors.py
+++ test/test_sensors.py
@@ -1,20 +1,20 @@
 import unittest
 from time import sleep
 
-from src.mqttTasks.sensorDevices.old.mq135 import SensorMQ135
+from src.mqttTasks.sensorDevices.scd41 import SensorSCD41
 from src.mqttTasks.sensorDevices.sds011 import SensorSDS011
 
 
 class MyTestCase(unittest.TestCase):
 
-    def test_sensor_mq135(self):
-        sensor_mq135 = SensorMQ135()
+    def test_sensor_scd41(self):
+        sensor_scd41 = SensorSCD41()
 
-        print("\nTest sensor mq135\n")
+        print("\nTest sensor scd41\n")
 
         for counter in range(10):
-            sensor_mq135.measure()
-            print(sensor_mq135.data)
+            sensor_scd41.measure()
+            print(sensor_scd41.data)
             sleep(1)
 
     def test_sensor_sds011(self):
